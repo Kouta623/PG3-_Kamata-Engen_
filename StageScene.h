ac6a241af1a03b5c7e58f070e4880d54b15eda92
Kouta623 <k023g1021@g.neec.ac.jp>
2024-07-05 15:54:31 +0900

    06_01

--- StageScene.h
+++ StageScene.h
@@ -2,8 +2,20 @@
 #include"IScene.h"
 //#include "dinput.h"
 #include"Novice.h"
+#include "Player.h"
+#include "InputHandle.h"
+#include "comand.h"
+
+
 class StageScene :public IScene
 {
+private:
+	InputHandle* inputHandle_ = nullptr;
+	IComand* iCommand_ = nullptr;
+	Player* player_ = nullptr;
+
+public:
+	StageScene();
 	void Init()override;
 	void Update(char* Keys, char *preKeys)override;
 	void Draw()override;
